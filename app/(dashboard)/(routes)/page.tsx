5f8113bdee23333aaf3c6baa9fb96502c455e167
marksikaundi <emmanuelmark117@gmail.com>
2024-08-06 01:28:51 +0200

    chore: Update .gitignore and package.json, add sign-in and sign-up pages

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h2 className="text-6xl">Hello world</h2>
+      <h2 className="text-6xl">This is a protected page</h2>
     </div>
   );
 }
