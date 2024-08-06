38909a99fc876bf3759b52325a2d1c32b40cf8e9
marksikaundi <emmanuelmark117@gmail.com>
2024-08-06 03:49:09 +0200

    feat: Add lazy loading for images in NavbarRoutes component

--- components/NavbarRoutes.tsx
+++ components/NavbarRoutes.tsx
@@ -1,12 +1,23 @@
 "use client"
 
 import { UserButton } from "@clerk/nextjs"
+import { usePathname, useRouter } from "next/navigation"
 
 
 export const NavbarRoutes = () => {
+
+    const pathname = usePathname()
+    const rounter = useRouter()
+
+    const isTeacherPage = pathname?.startsWith("/teacher")
+    const isPlayerPage = pathname?.includes("/chapter")
+
+
+
     return (
         <div className="flex gap-x-2 ml-auto">
             <UserButton/>
+
 
         </div>
     )
