d67454d2441f787f11fb15ba48f655206ca14d91
marksikaundi <emmanuelmark117@gmail.com>
2024-08-06 02:45:35 +0200

    chore: Add onClick functionality to SidebarItem component

--- app/(dashboard)/_components/sidebarItem.tsx
+++ app/(dashboard)/_components/sidebarItem.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { cn } from "@/lib/utils";
 import { LucideIcon } from "lucide-react";
 import { usePathname, useRouter } from "next/navigation";
 
@@ -17,5 +18,23 @@ export const SidebarItem = ({ icon, label, href }: SidebarItemProps) => {
     pathname == href ||
     pathname?.startsWith(`${href}/`);
 
-  return <div>SidebarItem</div>;
+  const onClick = () => {
+    router.push(href);
+  };
+
+  return (
+    <button
+      onClick={onClick}
+      type="button"
+      className={cn(
+        "flex items-center gap-x-2 text-slate-500 text-sm font-[500] pl-6 transition-all   hover:text-slate-600 hover:bg-slate-300/20  ",
+        isActive && "text-sky-700 bg-sky-200/20 hover:bg-sky-200/20  hover:text-sky-700  "
+      )}
+    >
+        <div className="flex items-center gap-x-2 py-4">
+            
+
+        </div>
+    </button>
+  );
 };
