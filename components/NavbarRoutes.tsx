c7c954446a3706671dc7d4d2ff42947a0cce86da
marksikaundi <emmanuelmark117@gmail.com>
2024-08-06 03:57:16 +0200

    feat: Add logout button to NavbarRoutes component

--- components/NavbarRoutes.tsx
+++ components/NavbarRoutes.tsx
@@ -2,6 +2,9 @@
 
 import { UserButton } from "@clerk/nextjs"
 import { usePathname, useRouter } from "next/navigation"
+import { Button } from "@/components/ui/button"
+import { LogOut } from "lucide-react"
+import Link from "next/link"
 
 
 export const NavbarRoutes = () => {
@@ -16,6 +19,18 @@ export const NavbarRoutes = () => {
 
     return (
         <div className="flex gap-x-2 ml-auto">
+            {isTeacherPage || isPlayerPage ? (
+                <Button size="sm" variant="ghost">
+                    <LogOut className="h-4 m-4 mr-2" />
+                    Exit
+                </Button>
+            ) : (
+                <Link href="/teacher/courses" >
+                <Button size="sm" variant="ghost">
+                    Teacher mode
+                </Button>
+                </Link>
+            )}
             <UserButton/>
 
 
