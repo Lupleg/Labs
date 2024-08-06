c280a9e6746b39d417196029ceb8e045e3394d80
marksikaundi <emmanuelmark117@gmail.com>
2024-08-06 02:28:33 +0200

    chore: Add Logo and Sidebar components to dashboard

--- app/(dashboard)/_components/sidebar.tsx
+++ app/(dashboard)/_components/sidebar.tsx
@@ -1,9 +1,17 @@
-export const  Sidebar = () => {
-    return (
-        <div className="h-full border-r flex flex-col overflow-y-auto bg-white shadow-sm">
-            Sidebar here
-        </div>
-    )
-}
+import { Logo } from "./logo";
+import { SidebarRoutes } from "./sidebarRoutes";
 
+export const Sidebar = () => {
+  return (
+    <div className="h-full border-r flex flex-col overflow-y-auto bg-white shadow-sm">
+      <div className="p-6">
+        <Logo/>
 
+      </div>
+      <div className="flex flex-col w-full">
+        <SidebarRoutes/>
+
+      </div>
+    </div>
+  );
+};
