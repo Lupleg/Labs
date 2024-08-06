bf113432d8b37fdbce78a544fa65843be0fa8e73
marksikaundi <emmanuelmark117@gmail.com>
2024-08-06 04:01:07 +0200

    feat: Add logout button to NavbarRoutes component

--- components/NavbarRoutes.tsx
+++ components/NavbarRoutes.tsx
@@ -20,10 +20,12 @@ export const NavbarRoutes = () => {
     return (
         <div className="flex gap-x-2 ml-auto">
             {isTeacherPage || isPlayerPage ? (
+                <Link href="/">
                 <Button size="sm" variant="ghost">
                     <LogOut className="h-4 m-4 mr-2" />
                     Exit
                 </Button>
+                </Link>
             ) : (
                 <Link href="/teacher/courses" >
                 <Button size="sm" variant="ghost">
