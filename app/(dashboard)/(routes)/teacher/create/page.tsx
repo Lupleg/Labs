9affb97a5b0d13b34ba5f7eb4732edeeab1ae6a5
marksikaundi <emmanuelmark117@gmail.com>
2024-08-06 04:51:22 +0200

    feat: Update CreatePage component and dependencies
    - Update CreatePage component to include axios and zod imports
    - Update package.json to include new dependencies: @hookform/resolvers, @radix-ui/react-label, axios, react-hook-form, and zod
    - Add Label and Input components to the UI folder
    - Add Form, FormItem, FormLabel, FormControl, FormDescription, FormMessage, and FormField components to the UI folder

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -1,11 +1,12 @@
-"use client"
+"use client";
+
+import axios from "axios";
+import * as z from "zod";
+
+import { zodResolver } from "@hookform/resolvers/zod";
 
 const CreatePage = () => {
-    return (  
-        <div>
-            Create course
-        </div>
-    );
-}
- 
+  return <div>Create course</div>;
+};
+
 export default CreatePage;
