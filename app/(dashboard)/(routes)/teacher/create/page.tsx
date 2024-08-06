ed7cedd9339c2b8113e7058ad951b290ab78ed1a
marksikaundi <emmanuelmark117@gmail.com>
2024-08-07 00:55:20 +0200

    feat: Update CreatePage component with form validation
    - Add react-hook-form and zod imports
    - Create form schema for validation
    - Implement useForm and useRouter hooks
    - Add Form, FormField, FormLabel, FormControl, FormDescription, and FormMessage components
    - Update CreatePage to include form rendering and default values

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -4,8 +4,38 @@ import axios from "axios";
 import * as z from "zod";
 
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import  {  useRouter } from "next/router";
+
+import {
+  Form,
+  FormField,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage
+
+} from "@/components/ui/form"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+
+const formSchema = z.object({
+  title: z.string().min(1, { 
+    message: "Title is required",
+  }),
+  
+});
 
 const CreatePage = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+    },
+  });
+  
+
+
   return <div>Create course</div>;
 };
 
