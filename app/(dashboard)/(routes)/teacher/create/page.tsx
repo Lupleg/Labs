6a1388a537c41f5de334f8c5994840996ce6a798
marksikaundi <emmanuelmark117@gmail.com>
2024-08-08 00:34:10 +0200

    feat: Update CreatePage component with form validation and default values

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -18,31 +18,26 @@ import {
 } from "@/components/ui/form"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
+import { resolve } from "path";
+
 
 const formSchema = z.object({
-  title: z.string().min(1, { 
-    message: "Title is required",
+  title: z.string().min(1,{
+    message: "Title is required"
   }),
-  
-});
+  })
 
 const CreatePage = () => {
+  
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    defaultValues: {
-      title: "",
-    },
   });
 
-  const { isSubitting, isValid } = form.formState;
-
-  const onSudmit = (values:z.infer<typeof formSchema>) =>{
-    console.log(values);
-  }
-  
-
-
-  return <div>Create course</div>;
-};
+  return (
+    <div>
+      Create page
+    </div>
+  )
+}
 
 export default CreatePage;
