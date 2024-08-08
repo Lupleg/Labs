a7aa13b5b3e7e99d51deb4bd5dcb2be37b257c61
marksikaundi <emmanuelmark117@gmail.com>
2024-08-08 02:17:34 +0200

    feat: Update CreatePage component with improved form validation and default values

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -5,7 +5,7 @@ import * as z from "zod";
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import  {  useRouter } from "next/router";
+import { useRouter } from "next/router";
 
 import {
   Form,
@@ -14,41 +14,41 @@ import {
   FormControl,
   FormDescription,
   FormMessage,
-  FormItem
-
-} from "@/components/ui/form"
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
+  FormItem,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { resolve } from "path";
-
+import Link from "next/link";
 
 const formSchema = z.object({
-  title: z.string().min(1,{
-    message: "Title is required"
+  title: z.string().min(1, {
+    message: "Title is required",
   }),
-  })
+});
 
 const CreatePage = () => {
-  
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       title: "",
-    }
+    },
   });
 
   const { isSubmitting, isValid } = form.formState;
 
   const onSubmit = (values: z.infer<typeof formSchema>) => {
     console.log(values);
-  }
+  };
 
   return (
     <div className="max-w-5xl mx-auto flex md:items-center md:justify-center h-full p-6">
       <div className="">
         <h1 className="text-2xl">Name your course</h1>
-        <p  className="text-sm text-slate-600"> What would you like to name your course?
-          Don&apos;t worry you can change this later.
+        <p className="text-sm text-slate-600">
+          {" "}
+          What would you like to name your course? Don&apos;t worry you can
+          change this later.
         </p>
         <Form {...form}>
           <form
@@ -56,29 +56,41 @@ const CreatePage = () => {
             className="space-y-8 mt-8"
           >
             <FormField
-            control={form.control}
-            name="title"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>
-                  Course title
-                </FormLabel>
-                <FormControl>
-                  <Input {...field}
-                  disabled={isSubmitting}
-                  placeholder="e.g 'Advanced TypeScript Techniques'"
-                   />
-                </FormControl>
-              </FormItem>
-            )}
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Course title</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      disabled={isSubmitting}
+                      placeholder="e.g 'Advanced TypeScript Techniques'"
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    What will you teach in this course?
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
             />
+            <div className="flex items-center gap-x-2">
+              <Link href="/">
+                <Button type="button" variant="ghost">
+                  Cancel
+                </Button>
+              </Link>
 
+              <Button type="submit" disabled={!isValid || isSubmitting}>
+                Continue
+              </Button>
+            </div>
           </form>
-
         </Form>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default CreatePage;
