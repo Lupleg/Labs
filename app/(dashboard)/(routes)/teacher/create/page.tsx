dcf99ad36f9e490652d8a3343e2f0a6f98033e9d
marksikaundi <emmanuelmark117@gmail.com>
2024-08-08 02:38:54 +0200

    CORE: Created toast

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -20,6 +20,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { resolve } from "path";
 import Link from "next/link";
+import toast from "react-hot-toast";
 
 const formSchema = z.object({
   title: z.string().min(1, {
@@ -43,7 +44,7 @@ const CreatePage = () => {
       const response = await axios.post("/api/courses", values);
       router.push(`/teacher/courses/${response.data.id}`);
     } catch {
-      console.log("Something went wrong");
+      toast.error("Something went wrong")
     }
   };
 
