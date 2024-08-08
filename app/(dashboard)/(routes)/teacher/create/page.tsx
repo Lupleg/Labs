31fd7e663dac5a8195bfe56880ec5297c6fc9366
marksikaundi <emmanuelmark117@gmail.com>
2024-08-08 02:10:12 +0200

    feat: Update CreatePage component with form validation and default values

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -13,7 +13,8 @@ import {
   FormLabel,
   FormControl,
   FormDescription,
-  FormMessage
+  FormMessage,
+  FormItem
 
 } from "@/components/ui/form"
 import { Button } from "@/components/ui/button"
@@ -46,6 +47,35 @@ const CreatePage = () => {
     <div className="max-w-5xl mx-auto flex md:items-center md:justify-center h-full p-6">
       <div className="">
         <h1 className="text-2xl">Name your course</h1>
+        <p  className="text-sm text-slate-600"> What would you like to name your course?
+          Don&apos;t worry you can change this later.
+        </p>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-8 mt-8"
+          >
+            <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>
+                  Course title
+                </FormLabel>
+                <FormControl>
+                  <Input {...field}
+                  disabled={isSubmitting}
+                  placeholder="e.g 'Advanced TypeScript Techniques'"
+                   />
+                </FormControl>
+              </FormItem>
+            )}
+            />
+
+          </form>
+
+        </Form>
       </div>
     </div>
   )
